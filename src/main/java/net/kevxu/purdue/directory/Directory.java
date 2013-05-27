43dd067b20de2b148fe65ac7334790903204deed
Kaiwen Xu <kevin@kevxu.net>
2013-05-27 01:05:15 -0400

    Adding address.

--- src/main/java/net/kevxu/purdue/directory/Directory.java
+++ src/main/java/net/kevxu/purdue/directory/Directory.java
@@ -2,6 +2,9 @@ package net.kevxu.purdue.directory;
 
 public class Directory {
 
+    private static final String ADDRESS = "ped.purdue.edu";
+    private static final String[] ADDRESS_FALLBACK = {"128.210.5.138", "128.210.5.137"};
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
